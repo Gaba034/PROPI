c3e9e2c4867d6b20cd1020801797e000145451ee
Gaba034 <gabiiinski@hotmail.com>
2022-01-25 11:51:14 -0300

    proh

---
+++ pro/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main style="padding : 0px 0px 0px">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+};
+</script>
+
+<style>
+
+</style>
